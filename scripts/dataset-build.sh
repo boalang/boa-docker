d5b8fb3e2a113139152c4c3f420d4a8c1c818469
Robert Dyer <rdyer@unl.edu>
2022-10-22 21:46:55 -0500

    update dataset build script to support multiple input projects

--- scripts/dataset-build.sh
+++ scripts/dataset-build.sh
@@ -2,8 +2,8 @@
 
 cd /compiler
 
-PROJECT="apache/rocketmq"
-PROJID="75164823"
+PROJECTS=( "apache/rocketmq" "apache/logging-log4j1" )
+PROJIDS=( "75164823" "206384" )
 
 # dont change below
 OUTPUTDIR="dataset-new"
@@ -12,44 +12,57 @@ JSONDIR="$OUTPUTDIR/json"
 REPODIR="$OUTPUTDIR/git"
 OUTDIR="$OUTPUTDIR/ds"
 
-IDPREFIX="${PROJID:0:1}/${PROJID:1:1}"
-
 changed="0"
 
-if [ ! -f $JSONDIR/$IDPREFIX/$PROJID.json ] ; then
-    mkdir -p $JSONDIR/$IDPREFIX
-    /bin/echo -n "[" > $JSONDIR/$IDPREFIX/$PROJID.json
-    wget -qO - "https://api.github.com/repos/$PROJECT" >> $JSONDIR/$IDPREFIX/$PROJID.json
-    echo "]" >> $JSONDIR/$IDPREFIX/$PROJID.json
-fi
+for i in "${!PROJECTS[@]}"; do
+    PROJECT=${PROJECTS[i]}
+    PROJID=${PROJIDS[i]}
 
-if [ ! -f $REPODIR/$IDPREFIX/$PROJID/HEAD ] ; then
-    mkdir -p $REPODIR/$IDPREFIX
-    git clone --bare https://github.com/$PROJECT.git $REPODIR/$IDPREFIX/$PROJID
-    changed="1"
-else
-    pushd .
-    cd $REPODIR/$IDPREFIX/$PROJID
-    HEAD=`cut -d' ' -f2 HEAD`
-    if [ -f $HEAD ] ; then
-        oldref=`cat refs/heads/main`
-    else
-        oldref=`grep "$HEAD" packed-refs`
+    echo "========================="
+    echo "updating $PROJECT ($PROJID)..."
+    echo "========================="
+
+    IDPREFIX="${PROJID:0:1}/${PROJID:1:1}"
+
+    if [ ! -f $JSONDIR/$IDPREFIX/$PROJID.json ] ; then
+        mkdir -p $JSONDIR/$IDPREFIX
+        /bin/echo -n "[" > $JSONDIR/$IDPREFIX/$PROJID.json
+        wget -qO - "https://api.github.com/repos/$PROJECT" >> $JSONDIR/$IDPREFIX/$PROJID.json
+        echo "]" >> $JSONDIR/$IDPREFIX/$PROJID.json
     fi
-    git fetch
-    if [ -f $HEAD ] ; then
-        newref=`cat refs/heads/main`
-    else
-        newref=`grep "$HEAD" packed-refs`
-    fi
-    popd
-    if [[ "$newref" != "$oldref" ]] ; then
+
+    if [ ! -f $REPODIR/$IDPREFIX/$PROJID/HEAD ] ; then
+        mkdir -p $REPODIR/$IDPREFIX
+        git clone --bare https://github.com/$PROJECT.git $REPODIR/$IDPREFIX/$PROJID
         changed="1"
+    else
+        pushd . > /dev/null
+        cd $REPODIR/$IDPREFIX/$PROJID
+        HEAD=`cut -d' ' -f2 HEAD`
+        if [ -f $HEAD ] ; then
+            oldref=`cat refs/heads/main`
+        else
+            oldref=`grep "$HEAD" packed-refs`
+        fi
+        git fetch
+        if [ -f $HEAD ] ; then
+            newref=`cat refs/heads/main`
+        else
+            newref=`grep "$HEAD" packed-refs`
+        fi
+        popd > /dev/null
+        if [[ "$newref" != "$oldref" ]] ; then
+            changed="1"
+        fi
     fi
-fi
+done
 
 if [[ "$changed" == "1" ]] ; then
     rm -Rf $OUTDIR
-    ./boa.sh -g -inputJson $JSONDIR -inputRepo $REPODIR -output $OUTDIR -debug
+    ./boa.sh -g -skip 0 -inputJson $JSONDIR -inputRepo $REPODIR -output $OUTDIR -debug
+    echo ""
     echo "dataset was rebuilt - you need to reinstall it into HDFS. Run 'dataset-install.sh' next."
+else
+    echo ""
+    echo "dataset was not updated - none of the input projects changed"
 fi
